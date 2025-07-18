4f3f8a718c529845fc75fe3a88e8353a47fa1fcb
zhstzzy <84577584+zhstzzy@users.noreply.github.com>
2025-07-18 15:55:58 +0800

    Update 99-custom.sh

--- raspberrypi/files/etc/uci-defaults/99-custom.sh
+++ raspberrypi/files/etc/uci-defaults/99-custom.sh
@@ -132,7 +132,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by wukongdaily"
+NEW_DESCRIPTION="Compiled by copyz"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
